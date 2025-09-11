e3c5482cdeb68afc48127a7c806012809204267c
ngaptpwork-cmd <ngaptpwork@gmail.com>
2025-09-11 17:08:49 +0700

    chapter4

---
+++ Chapter_4/4-1_case.py
@@ -0,0 +1,4 @@
+pizza_list = ['Hawaian', 'Bacon', 'Cheese']
+for pizza in pizza_list:
+    print(f"I ate {pizza} at least twice.")
+print("I really like pizza, I wanna taste all of pizza on the world")
---
+++ Chapter_4/4-2_case.py
@@ -0,0 +1,6 @@
+pets = ['dog','cat','turtle','fish','hamster']
+print("My house have many pets, that is")
+for pet in pets:
+    print(f"{pet}")
+    print(f"{pet} is a cute animal, which can treat yourself\n")
+print("Any of these animals would be a good friend!")
